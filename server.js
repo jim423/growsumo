36a9ff62963239e6cc92ac3bb0837a2e22c6e837
Jim <jim423@live.cn>
2017-06-26 22:23:53 -0400

    fix persistency on new client connections and fix loadNewTodo() in server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const server = require('socket.io')();
-const firstTodos = require('./data');
 const Todo = require('./todo');
+const fs = require('fs');
 
 server.on('connection', (client) => {
     // This is going to be our fake 'database' for this application
     // Parse all default Todo's from db
+
+    // Read data from data.json
+    const firstTodos = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
     // FIXME: DB is reloading on client refresh. It should be persistent on new client
     // connections from the last time the server was run...
@@ -19,10 +22,10 @@ server.on('connection', (client) => {
     }
 
     // Sends a message to the client to add only the new todo
-    const loadNewTodo = () => {
-      const newTodo = [];
-      newTodo.push(DB.pop());
-      server.emit('load', newTodo);
+    const loadNewTodo = (newTodo) => {
+      const newTodoList = [];
+      newTodoList.push(newTodo);
+      server.emit('load', newTodoList);
     }
 
     // Accepts when a client makes a new todo
@@ -32,10 +35,13 @@ server.on('connection', (client) => {
         // Push this newly created todo to our database
         DB.push(newTodo);
 
+        // Add new todo to data.json
+        fs.writeFileSync('data.json', JSON.stringify(DB));
+
         // Send the latest todos to the client
         // FIXME: This sends all todos every time, could this be more efficient?
         // reloadTodos();
-        loadNewTodo();
+        loadNewTodo(newTodo);
     });
 
     // Send the DB downstream on connect
