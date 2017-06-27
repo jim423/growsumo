088bcd4b2cc75d9b118fbe68f0d293c06b1d7b18
Jim <jim423@live.cn>
2017-06-27 03:53:58 -0400

    final commit, add feature of adding/deleting task for both todo and complete list

--- client.js
+++ client.js
@@ -1,5 +1,4 @@
 const server = io('http://localhost:3003/');
-const list = document.getElementById('todo-list');
 
 // NOTE: These are all our globally scoped functions for interacting with the server
 // This function adds a new todo from the input
@@ -18,15 +17,98 @@ function add() {
     input.focus();
 }
 
+function showCompletedTask() {
+  // Show completed tasks' list
+  $('.complete-task').toggle();
+}
+
+// This function selects all todos
+function selectAll() {
+  $("input[name='todo']").prop('checked', true);
+}
+
+// This functions unselects all todos
+function unselectAll() {
+  $("input[name='todo']").prop('checked', false);
+}
+
+// This functions completes todo
+function completeTodo() {
+  let selectedTodo = [];
+
+  // Get all selected todos
+  $("input[name='todo']:checked").each(function(index, todo) {
+    selectedTodo.push($(todo).val());
+  });
+
+  // Emit the complete todo as some data to the server
+  server.emit('complete', selectedTodo);
+}
+
+// This function delete todo
+function deleteTodo() {
+  let selectedTodo = [];
+
+  // Get all selected todos
+  $("input[name='todo']:checked").each(function(index, todo) {
+    selectedTodo.push($(todo).val());
+  });
+
+  // Emit the deleted todo as some data to the server
+  server.emit('delete', selectedTodo);
+}
+
+// This function selects all completed tasks
+function selectAllTask() {
+  $("input[name='task']").prop('checked', true);
+}
+
+// This functions unselects all tasks
+function unselectAllTask() {
+  $("input[name='task']").prop('checked', false);
+}
+
+// This function deletes completed task
+function deleteTask() {
+  let selectedTask = [];
+
+  // Get all selected todos
+  $("input[name='task']:checked").each(function(index, task) {
+    selectedTask.push($(task).val());
+  });
+
+  // Emit the deleted task as some data to the server
+  server.emit('deleteTask', selectedTask);
+}
+
 function render(todo) {
-    const listItem = document.createElement('li');
-    const listItemText = document.createTextNode(todo.title);
-    listItem.appendChild(listItemText);
-    list.append(listItem);
+
+    // Append li tag with input inside it for todo.
+    $('#todo-list').append('<li class="list-group-item" >'
+    +'<input type="checkbox" name="todo" value="' + todo.title + '" /> '
+    + todo.title + '</li>');
+
+}
+
+function renderCompletedTask(todo) {
+
+    // Append li tag with input inside it for todo.
+    $('#complete-todo-list').append('<li class="list-group-item" >'
+    +'<input type="checkbox" name="task" value="' + todo.title + '" /> '
+    + todo.title + '</li>');
+
 }
 
 // NOTE: These are listeners for events from the server
 // This event is for (re)loading the entire list of todos from the server
-server.on('load', (todos) => {
+server.on('load', (t) => {
+    $('#todo-list').empty();
+    $('#complete-todo-list').empty();
+    t.todo.forEach((todo) => render(todo));
+    t.complete.forEach((todo) => renderCompletedTask(todo));
+});
+
+// This event is for adding a new todo
+server.on('add', (todos) => {
     todos.forEach((todo) => render(todo));
 });
--- server.js
+++ server.js
@@ -6,26 +6,53 @@ server.on('connection', (client) => {
     // This is going to be our fake 'database' for this application
     // Parse all default Todo's from db
 
-    // Read data from data.json
-    const firstTodos = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    // Read data from todo.json
+    const firstTodos = JSON.parse(fs.readFileSync('todo.json', 'utf8'));
+
+    // Read data from complete.json
+    const firstCompletedTodos = JSON.parse(fs.readFileSync('complete.json', 'utf8'));
 
     // FIXME: DB is reloading on client refresh. It should be persistent on new client
     // connections from the last time the server was run...
-    const DB = firstTodos.map((t) => {
+    const todoDB = firstTodos.map((t) => {
+        // Form new Todo objects
+        return new Todo(title=t.title);
+    });
+
+    const completedDB = firstCompletedTodos.map((t) => {
         // Form new Todo objects
         return new Todo(title=t.title);
     });
 
     // Sends a message to the client to reload all todos
     const reloadTodos = () => {
-        server.emit('load', DB);
+        server.emit('load', {
+          todo: todoDB,
+          complete: completedDB
+        });
     }
 
     // Sends a message to the client to add only the new todo
     const loadNewTodo = (newTodo) => {
       const newTodoList = [];
       newTodoList.push(newTodo);
-      server.emit('load', newTodoList);
+      server.emit('add', newTodoList);
+    }
+
+    // Sends a message to the client to remove completed todos
+    const completedTodo = (t) => {
+      server.emit('load', {
+        todo: t.todo,
+        complete: t.complete
+      });
+    }
+
+    // Sends a message to the client to remove deleted todos
+    const deletedTodo = (t) => {
+      server.emit('load', {
+        todo: t.todo,
+        complete: t.complete
+      });
     }
 
     // Accepts when a client makes a new todo
@@ -33,15 +60,83 @@ server.on('connection', (client) => {
         // Make a new todo
         const newTodo = new Todo(title=t.title);
         // Push this newly created todo to our database
-        DB.push(newTodo);
+        todoDB.push(newTodo);
 
-        // Add new todo to data.json
-        fs.writeFileSync('data.json', JSON.stringify(DB));
+        // Add new todo to todo.json
+        fs.writeFileSync('todo.json', JSON.stringify(todoDB));
 
         // Send the latest todos to the client
         // FIXME: This sends all todos every time, could this be more efficient?
         // reloadTodos();
         loadNewTodo(newTodo);
+    });
+
+    // Accepts when a client completes todo
+    client.on('complete', (t) => {
+        // Remove completed todos from DB
+        t.forEach((completedTodo) => {
+          todoDB.forEach((todo, i) => {
+            if (todo.title === completedTodo) {
+              todoDB.splice(i, 1);
+              const newTodo = new Todo(title=completedTodo);
+              completedDB.push(newTodo);
+            }
+          });
+        });
+
+        // Add complete todo to complete.json
+        fs.writeFileSync('complete.json', JSON.stringify(completedDB));
+
+        // Save new todo list to todo.json
+        fs.writeFileSync('todo.json', JSON.stringify(todoDB));
+
+        // Send latest todo to the client
+        completedTodo({
+          todo: todoDB,
+          complete: completedDB
+        });
+    });
+
+    // Accepts when a client deletes todo
+    client.on('delete', (t) => {
+        // Remove deleted todos from DB
+        t.forEach((detetedTodo) => {
+          todoDB.forEach((todo, i) => {
+            if (todo.title === detetedTodo) {
+              todoDB.splice(i, 1);
+            }
+          });
+        });
+
+        // Save new todo list to todo.json
+        fs.writeFileSync('todo.json', JSON.stringify(todoDB));
+
+        // Send the latest todos to the client
+        deletedTodo({
+          todo: todoDB,
+          complete: completedDB
+        });
+    });
+
+    // Accepts when a client deletes completed task
+    client.on('deleteTask', (t) => {
+        // Remove deleted todos from DB
+        t.forEach((detetedTask) => {
+          completedDB.forEach((task, i) => {
+            if (task.title === detetedTask) {
+              completedDB.splice(i, 1);
+            }
+          });
+        });
+
+        // Save new todo list to todo.json
+        fs.writeFileSync('complete.json', JSON.stringify(completedDB));
+
+        // Send the latest todos to the client
+        deletedTodo({
+          todo: todoDB,
+          complete: completedDB
+        });
     });
 
     // Send the DB downstream on connect
